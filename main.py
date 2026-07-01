5fd863bcd2eb64d22b932ba9f44568729e36bb04
Your Name <you@example.com>
2026-07-01 13:41:09 +0100

    put css into static dir

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 model = joblib.load("model.pkl")
